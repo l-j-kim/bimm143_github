9e31c39afc79e792ce0a6f15f3a9fb7ac2043089
l-j-kim <ljkim@ucsd.edu>
2025-06-05 10:26:47 -0700

    add class 4, 8-10

---
+++ class04/class04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Intro to R"
+#' author: "Leah Kim A16973745"
+#' date: "April 10, 2025"
+#' ---
+
+#My first R script
+x <- 1:50
+
+plot(x, sin(x))
+plot(x, sin(x), typ="l", col="red",lwd=3, xlab="Silly x axis", ylab="Sensible y axis")
+
